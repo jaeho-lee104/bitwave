48bbcd2318ebf103ae9426bdf63e528cea58174f
jaeho.lee104 <jaeho.lee104@webtoonscorp.com>
2021-03-21 04:43:00 +0900

    feat: BottomNavigation 심플 UI 적용

--- app/src/main/java/com/ecohouse/bitwave/MainActivity.kt
+++ app/src/main/java/com/ecohouse/bitwave/MainActivity.kt
@@ -2,10 +2,20 @@ package com.ecohouse.bitwave
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.ecohouse.bitwave.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener {
+            println("${it.itemId} clicked")
+            true
+        }
     }
 }
